153587b87345fb78ce95d55c8df8bfb5c5166ab1
zlodiak <prozaik81-2@yandex.ru>
2015-09-23 11:13:10 +0300

    mail end

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -3,7 +3,14 @@ class ReviewMailer < ApplicationMailer
  
   def welcome_email(review)
     @review = review
+    #attachments['castle.jpg'] = File.read(@review.image.url(:thumb))
+    attachments['castle.jpg'] = File.read(Rails.root.join('public/castle.jpg'))
+    #attachments['review.jpg'].inline = File.read(Rails.root.join(@review.image.url(:thumb)))
     mail(to: 'prozaik81-2@yandex.ru', subject: 'New review create')
   end  
 end
+ 
+
+
+ 
  
