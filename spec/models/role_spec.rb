9fff43b4de1a75bf947fb809bcb6c1f20fdd3154
zlodiak <prozaik81-2@yandex.ru>
2015-09-28 15:10:59 +0300

    make factories and specs for user admin ok

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "user with role admin" do  
+    before(:each) do
+      @user = FactoryGirl.create(:user, :admin)
+      @ability = Ability.new(@user)
+    end
+  
+    it "can show review" do
+      expect(@ability).to be_able_to(:show, Review)
+    end
+
+    it "cannot delete review" do
+      expect(@ability).to be_able_to(:delete, Review)
+    end  
+  end  
+
 end
