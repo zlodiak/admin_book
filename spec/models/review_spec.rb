78c4fc262e8f41f43289a91e7a095ba288ea33ca
zlodiak <prozaik81-2@yandex.ru>
2015-09-30 12:51:38 +0300

    reviews models ok

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -12,37 +12,45 @@ describe Review, type: :model do
   end    
 
   describe 'name' do
-    it "is invalid without name" do
-      expect(FactoryGirl.build(:review, name: nil)).not_to be_valid
-    end  
+    context "invalid" do  
+      it "is invalid without name" do
+        expect(FactoryGirl.build(:review, name: nil)).not_to be_valid
+      end  
 
-    it "is invalid with short name" do
-      expect(FactoryGirl.build(:review, name: 'q')).not_to be_valid
+      it "is invalid with short name" do
+        expect(FactoryGirl.build(:review, name: 'q')).not_to be_valid
+      end      
     end      
 
-    it "is valid with duplicate name" do
-      FactoryGirl.create(:review, name: 'qwerty')
-      expect(FactoryGirl.build(:review, name: 'qwerty')).to be_valid
+    context "valid" do  
+      it "is valid with duplicate name" do
+        FactoryGirl.create(:review, name: 'qwerty')
+        expect(FactoryGirl.build(:review, name: 'qwerty')).to be_valid
+      end 
     end 
   end   
 
   describe 'image' do
-    it "is valid when image is empty" do
-      expect(FactoryGirl.build(:review, 
-        image_file_name: nil
-      )).to be_valid
+    context "valid" do
+      it "is valid when image is empty" do
+        expect(FactoryGirl.build(:review, 
+          image_file_name: nil
+        )).to be_valid
+      end     
+
+      it "is valid when image content_type is: image/*" do
+        expect(FactoryGirl.build(:review, 
+          image_content_type: 'image/png'
+        )).to be_valid
+      end 
+    end
+
+    context "invalid" do  
+      it "is invalid when image content_type is not: image/*" do
+        expect(FactoryGirl.build(:review, 
+          image_content_type: 'video/png'
+        )).not_to be_valid
+      end         
     end     
-
-    it "is valid when image content_type is: image/*" do
-      expect(FactoryGirl.build(:review, 
-        image_content_type: 'image/png'
-      )).to be_valid
-    end 
-
-    it "is invalid when image content_type is not: image/*" do
-      expect(FactoryGirl.build(:review, 
-        image_content_type: 'video/png'
-      )).not_to be_valid
-    end         
   end     
 end
